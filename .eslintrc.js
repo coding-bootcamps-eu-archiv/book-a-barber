03fb9b8261efda35ea9c67716849ceb6f33248c9
Enrico Carruba <carrubaenrico@gmail.com>
2021-11-17 15:59:27 +0100

    add fake data structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,11 @@ module.exports = {
     "vue/multi-word-component-names": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -1,8 +1,59 @@
 <template>
-  <article>
-    <h1>Wann möchtest du von uns Besucht zu Werden und von Wem?</h1>
-    <label for="calender">Time</label>
-    <input type="text" id="calender" />
-    <router-link to="/Payment">Up to pay!!!</router-link>
-  </article>
+  <section>
+    <article>
+      <h1>Termin auswählen</h1>
+      <ul>
+        <li class="days">Montag</li>
+        <li class="days">Dienstag</li>
+        <li class="days">Mittwoch</li>
+        <li class="days">Donnerstag</li>
+        <li class="days">Freitag</li>
+        <li class="days">Samstag</li>
+        <li class="days">Sonntag</li>
+        <li
+          class="appointments"
+          v-for="appointment in availableAppointments"
+          :key="appointment"
+        >
+          {{ appointment.monday }}
+          {{ appointment.thuesday }}
+          {{ appointment.wednesday }}
+        </li>
+      </ul>
+
+      <router-link to="/Payment"
+        ><button class="button-next-page">
+          Auswahl bestätigen
+        </button></router-link
+      >
+    </article>
+  </section>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    availableAppointments: [
+      { monday: ["10:00", "11.00"] },
+      { thuesday: [10.0, 12.0, 13.0, 14.0, 17.0] },
+      { wednesday: [9.0, 12.0, 15.0, 16.0, 18.0] },
+    ],
+  }),
+};
+</script>
+
+<style>
+.button-next-page {
+  position: absolute;
+  bottom: 2rem;
+}
+
+.days {
+  list-style: none;
+  display: inline-block;
+}
+
+.appointments {
+  list-style: none;
+}
+</style>

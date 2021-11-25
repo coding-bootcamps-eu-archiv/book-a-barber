2ccf3f68d248f2a68055165356a9f4ad5e4845dd
vanessawa <81361169+vanessawa@users.noreply.github.com>
2021-11-25 17:26:41 +0100

    style: add custom-properties and unity img

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@
 </template>
 
 <style>
+html {
+  --btn-background-clr: rgb(181, 181, 181);
+  --btn-border: rgb(131, 131, 131) solid 2px;
+  --btn-padding: 0.3rem 0.6rem;
+  --btn-hover-clr: rgb(131, 131, 131);
+}
+
 .nav-leiste {
   display: flex;
   align-items: center;
@@ -24,6 +31,24 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  position: relative;
+}
+
+#app:after {
+  content: " ";
+  position: absolute;
+  background-image: url("https://baerte.info/wp-content/uploads/2019/06/barber.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 50%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
 }
 
 nav {
